4b1f8843ef73d8be0d16fd1c9ee5085f2c99cd77
Sam Rodriguez <sr_odriguez9@yahoo.com>
2021-11-11 20:02:15 -0600

    war card game

---
+++ Shared/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  Shared
+//
+//  Created by Sam Rodriguez on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
